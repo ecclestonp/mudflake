5dc7fb2ff9cd3444aea71147c3d45bc7d7711f06
Ryan Manecke <rbm0003@uah.edu>
2016-02-22 19:00:19 -0600

    Changed to class from struct

--- Team_8_CS499/Team_8_CS499/Instructor.h
+++ Team_8_CS499/Team_8_CS499/Instructor.h
@@ -2,15 +2,13 @@
 //CS499 Senior Project
 //Ryan Manecke, Jeffrey Webb, Paul Eccleston
 
-#ifndef INSTRUCTOR_H
-#define INSTRUCTOR_H
+#pragma once
 
-//Define structure for teachers
-struct Instructor
+//Define class for teachers
+class Instructor
 {
-	char instructorName[100];		//Name of instructor
-	bool hasPreference;			//1 if there is a prefence, 0 if not
-	char preference[100];			//NULL if no preference, contains string of preference if there is one
+	public:
+		string instructorName;		//Name of instructor
+		bool hasPreference;			//1 if there is a prefence, 0 if not
+		string preference;			//NULL if no preference, contains string of preference if there is one
 };
-
-#endif
